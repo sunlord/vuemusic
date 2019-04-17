44888c757a2e782719513a7bbd8cba36985da453
sunlord <gj2011sd2011@163.com>
2019-04-17 13:04:42 +0800

    beta0.4.17.2
    1.囊括了标题导航栏四个组件

--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
+import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
-// import router from './router'
+import router from './router'
+import fastclick from 'fastclick'
 
 import 'common/stylus/index.styl'
 
-// Vue.config.productionTip = false
+fastclick.attach(document.body) /* 将fastclick Attach到整个body内，这样整个body内的点击没有300ms的延迟了 */
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  render: h => h(App)
+  render: h => h(App),
+  router
 })
